c7d183bcf6d4e2a1c1bef6b8bb4c4a4b02865fef
larissa de oliveira <larissaoliveira3zb@gmail.com>
2024-10-10 14:53:49 -0300

    Biblioteca

--- Program.cs
+++ Program.cs
@@ -6,6 +6,23 @@ class Program
 {
     static List<Livro> catalogo = new List<Livro>();
     {
-        static void 
+        static void Main(string[] args)
+        {
+            CarregarCatalago();
+            while(true)
+            {
+             Console.Clear();
+             Console.WriteLine("n/Menu Biblioteca:");
+             Console.WriteLine("1. Cadastrar livro");
+             Console.WriteLine("2. Ver catálogo");
+             Console.WriteLine("3. Emprestar livro");
+             Console.WriteLine("4. Devolver livro");
+             Console.WriteLine("5. Sair e salvar");
+
+
+            }
+        }
+
+
     }
 }
